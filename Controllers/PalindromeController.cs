9437a546336da253d6b9abd557db37f44e9cb3ba
lld21 <63442442+lld21@users.noreply.github.com>
2020-04-10 01:40:39 -0500

    Commiting files for Review

---
+++ Controllers/PalindromeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/Palindrome")]
+    public class PalindromeController : ApiController
+    {
+        [ResponseType(typeof(bool))]
+        [HttpPost]
+        [Route("IsPalindrome")]
+        public async Task<IHttpActionResult> IsPalindrome([FromBody] string word)
+        {
+            try
+            {
+                var orig = word;
+                Array.Reverse(word.ToArray());
+                Console.WriteLine(word);
+                return Ok(orig.Equals(word));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+           
+        }
+       
+    }
+}
